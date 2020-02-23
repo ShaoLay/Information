7ce6f509c8141503e61fe33c554f645165514f32
lay <javs_shao@163.com>
2020-02-23 15:56:53 +0800

    网站图标展示

--- information/info/modules/index/views.py
+++ information/info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from . import index_blu
 
@@ -6,3 +6,7 @@ from . import index_blu
 @index_blu.route('/')
 def index():
     return render_template('news/index.html')
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')

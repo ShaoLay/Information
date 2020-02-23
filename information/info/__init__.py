8a072baa00f1f22ad8a98180ebd591f7f3f537d6
lay <javs_shao@163.com>
2020-02-23 17:50:18 +0800

    登录注册模块蓝图

--- information/info/__init__.py
+++ information/info/__init__.py
@@ -27,6 +27,8 @@ def create_app(config_name):
     # 注册蓝图
     from info.modules.index import index_blu
     app.register_blueprint(index_blu)
+    from info.modules.passport import passport_blu
+    app.register_blueprint(passport_blu)
 
     return app
 

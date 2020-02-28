b2e10f095d447b3a92c6c2db6740da1517ac29b2
lay <javs_shao@163.com>
2020-02-28 16:50:43 +0800

    首页右上角内容逻辑显示

--- information/info/modules/index/views.py
+++ information/info/modules/index/views.py
@@ -1,11 +1,25 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 from . import index_blu
+from ...models import User
 
 
 @index_blu.route('/')
 def index():
-    return render_template('news/index.html')
+    """
+    主页
+    :return:
+    """
+    # 获取到当前登录用户的id
+    user_id = session.get("user_id")
+    # 通过id获取登录用户信息
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    return render_template('news/index.html', data={"user_info":user.to_dict() if user else None})
 
 @index_blu.route('/favicon.ico')
 def favicon():

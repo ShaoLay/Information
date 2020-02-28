e16951956b3fad28f87833bc69d1e5c1d634bf7c
lay <javs_shao@163.com>
2020-02-28 16:23:05 +0800

    登录前后端实现

--- information/info/modules/passport/views.py
+++ information/info/modules/passport/views.py
@@ -1,3 +1,4 @@
+import datetime
 import errno
 import random
 import re
@@ -147,4 +148,39 @@ def register():
     session['nick_name'] = user.nick_name
     session['mobile'] = user.mobile
     # 6. 返回注册信息
+    return jsonify(errno=RET.OK, errmsg="OK")
+
+@passport_blu.route('/login', methods=["POST"])
+def login():
+    """
+    登录
+    :return:
+    """
+    # 1. 获取参数和判断是否有值
+    json_data = request.json
+    mobile = json_data.get("mobile")
+    password = json_data.get("password")
+    if not all([mobile, password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不全")
+    try:
+        user = User.query.filter_by(mobile=mobile).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据错误！!")
+    if not user:
+        return jsonify(errno=RET.USERERR, errmsg="用户不存在！")
+    # 3. 校验密码
+    if not user.password == password:
+        return jsonify(errno=RET.PWDERR, errmsg="密码错误！")
+    # 4. 保存用户登录状态
+    session["user_id"] = user.id
+    session["nick_name"] = user.nick_name
+    session["mobile"] = user.mobile
+    # 记录用户最后一次登录时间
+    user.last_login = datetime.datetime.now()
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+    # 5. 登录成功
     return jsonify(errno=RET.OK, errmsg="OK")
--- information/info/static/news/js/main.js
+++ information/info/static/news/js/main.js
@@ -105,22 +105,41 @@ $(function(){
 
     // TODO 登录表单提交
     $(".login_form_con").submit(function (e) {
-        e.preventDefault()
-        var mobile = $(".login_form #mobile").val()
-        var password = $(".login_form #password").val()
+    e.preventDefault()
+    var mobile = $(".login_form #mobile").val()
+    var password = $(".login_form #password").val()
 
-        if (!mobile) {
-            $("#login-mobile-err").show();
-            return;
+    if (!mobile) {
+        $("#login-mobile-err").show();
+        return;
+    }
+
+    if (!password) {
+        $("#login-password-err").show();
+        return;
+    }
+
+    var params = {
+        "mobile": mobile,
+        "password": password,
+    }
+
+    $.ajax({
+        url:"/passport/login",
+        method: "post",
+        data: JSON.stringify(params),
+        contentType: "application/json",
+        success: function (resp) {
+            if (resp.errno == "0") {
+                // 刷新当前界面
+                location.reload();
+            }else {
+                $("#login-password-err").html(resp.errmsg)
+                $("#login-password-err").show()
+            }
         }
-
-        if (!pasfsword) {
-            $("#login-password-err").show();
-            return;
-        }
-
-        // 发起登录请求
     })
+})
 
 
     // TODO 注册按钮点击

f4e5cd93af9bc0816024382e3f20f0e8147f4efb
lay <javs_shao@163.com>
2020-02-28 18:11:14 +0800

    新闻首页_点击排行展示

--- information/info/__init__.py
+++ information/info/__init__.py
@@ -38,6 +38,8 @@ def create_app(config_name):
     app.register_blueprint(index_blu)
     from info.modules.passport import passport_blu
     app.register_blueprint(passport_blu)
+    from info.utils.common import do_index_class
+    app.add_template_filter(do_index_class, "index_class")
 
     return app
 
--- information/info/modules/index/views.py
+++ information/info/modules/index/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, current_app, session
 
 from . import index_blu
-from ...models import User
+from ... import constants
+from ...models import User, News
 
 
 @index_blu.route('/')
@@ -19,7 +20,19 @@ def index():
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
-    return render_template('news/index.html', data={"user_info":user.to_dict() if user else None})
+    news_list = None
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+    click_news_list = []
+    for news in news_list if news_list else []:
+        click_news_list.append(news.to_basic_dict())
+        data = {
+            "user_info":user.to_dict() if user else None,
+            "click_news_list": click_news_list,
+        }
+    return render_template('news/index.html', data=data)
 
 @index_blu.route('/favicon.ico')
 def favicon():

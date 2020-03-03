c0e36520718d90d1a1caf567a3a68b9641c67b78
lay <javs_shao@163.com>
2020-03-03 18:34:18 +0800

    详情页新闻数据展示(点击排行部分未完成)

--- information/info/modules/news/views.py
+++ information/info/modules/news/views.py
@@ -1,14 +1,39 @@
-from flask import render_template, session, current_app, g
+from flask import render_template, session, current_app, g, abort
 
 from . import news_blu
-from ...models import User
+from ... import constants, db
+from ...models import News
 from ...utils.common import user_login_data
 
 
-@news_blu.route('/<int:news_id>')
+@news_blu.route('/detail/<int:news_id>')
 @user_login_data
 def news_detail(news_id):
+    news_clicks = []
+    try:
+        news_clicks = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+    # 查询新闻详情
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        abort(404)
+
+    if not news:
+        # 返回数据未找到的页面
+        abort(404)
+    # 累加点击量
+    news.clicks += 1
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
     data = {
-        "user_info":g.user.to_dict() if g.user else None,
+        "news": news.to_dict(),
+        "user_info": g.user.to_dict() if g.user else None,
+        'news_clicks':news_clicks,
     }
     return render_template('news/detail.html', data=data)

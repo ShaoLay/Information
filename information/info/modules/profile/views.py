d0262875f0ab1b4b40b6eeebac9a3ad96bb5f7e2
lay <javs_shao@163.com>
2020-03-23 17:32:18 +0800

    我的关注页面前后端实现

--- information/info/modules/profile/views.py
+++ information/info/modules/profile/views.py
@@ -279,3 +279,37 @@ def news_list():
         "current_page": current_page,
     }
     return render_template('news/user_news_list.html', data=data)
+
+@profile_blu.route('/user_follow')
+@user_login_data
+def user_follow():
+    # 获取页数
+    p = request.args.get("p", 1)
+    try:
+        p = int(p)
+    except Exception as e:
+        current_app.logger.error(e)
+        p = 1
+
+    user = g.user
+
+    follows = []
+    current_page = 1
+    total_page = 1
+    try:
+        paginate = user.followed.paginate(p, constants.USER_FOLLOWED_MAX_COUNT, False)
+        # 获取当前页数据
+        follows = paginate.items
+        # 获取当前页
+        current_page = paginate.page
+        # 获取总页数
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    user_dict_li = []
+
+    for follow_user in follows:
+        user_dict_li.append(follow_user.to_dict())
+    data = {"users": user_dict_li, "total_page": total_page, "current_page": current_page}
+    return render_template('news/user_follow.html', data=data)
